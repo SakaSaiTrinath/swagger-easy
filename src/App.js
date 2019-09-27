73a368e31738a00e57b024a3a2f13a5ab28cd564
kishore2486 <ramkishore157359@gmail.com>
2019-09-27 16:58:23 +0530

    added eslint

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
 import React from "react";
-import { Form, Button, Card } from "react-bootstrap";
-
+import Header from './components/Header';
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      App
-      <Card style={{ width: "20rem" }}>
-        <Card.Body>
-          <Form>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
-
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
-        </Card.Body>
-      </Card>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
